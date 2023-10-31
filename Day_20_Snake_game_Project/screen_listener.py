236a8954c0c5fedbb18e365a07e61ba6f57d473a
Akash Chandel <akashsingh12345@gmail.com>
2023-10-31 12:27:51 +0530

    Event Listener added to head of the snake

---
+++ Day_20_Snake_game_Project/screen_listener.py
@@ -0,0 +1,24 @@
+
+# Move Left
+def head_north(a_turtle):
+    a_turtle.setheading(90)
+    a_turtle.forward(10)
+
+
+def head_east(a_turtle):
+    a_turtle.setheading(0)
+    a_turtle.forward(10)
+
+
+def head_west(a_turtle):
+    a_turtle.setheading(180)
+    a_turtle.forward(10)
+
+
+def head_south(a_turtle):
+    a_turtle.setheading(270)
+    a_turtle.forward(10)
+
+
+
+
--- Day_20_Snake_game_Project/snake_game.py
+++ Day_20_Snake_game_Project/snake_game.py
@@ -1,11 +1,14 @@
 from turtle import Turtle, Screen
+from screen_listener import head_east, head_west, head_north, head_south
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=600, height=600)
 screen.title("Feed The SNAKE")
+screen.listen()
 
-# TODO 1: create 3 turtles 
+
+# TODO 1: create 3 turtles
 initial_x = 0
 initial_y = 0
 all_turtles = []
@@ -18,6 +21,10 @@ for i in range(3):
     initial_x += 20
 
 
+screen.onkey(lambda :head_north(tur), "Up")
+screen.onkey(lambda :head_west(tur), "Left")
+screen.onkey(lambda :head_east(tur), "Right")
+screen.onkey(lambda :head_south(tur), "Down")
 
 
 
