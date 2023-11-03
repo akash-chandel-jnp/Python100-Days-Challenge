483725d3e0fad1ecdbde5db4ce8721a26b46e0cb
Akash Chandel <akashsingh12345@gmail.com>
2023-11-04 00:06:19 +0530

    snake movement controls done

--- Day_20_b_snakeGame_by_taking_help/snake.py
+++ Day_20_b_snakeGame_by_taking_help/snake.py
@@ -8,8 +8,9 @@ class Snake:
         self.shape = shape
         self.color = color
         self.segments = []
-        self.body_length = len(self.segments)
         self.create_body()
+        self.head = self.segments[0]
+        self.body_length = len(self.segments)
 
     def create_body(self):
         for position in STARTING_POSITIONS:
@@ -18,10 +19,10 @@ class Snake:
             new_segment.penup()
             new_segment.setposition(position)
             self.segments.append(new_segment)
-        self.body_length += 3
+        # self.body_length += 3
 
     def move(self):
-        for seg_num in range(self.body_length - 1, 0, -1):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
@@ -29,13 +30,17 @@ class Snake:
 
 
     def up(self):
-        self.segments[0].setheading(90)
+        if self.head.heading() != 270:
+            self.head.setheading(90)
 
     def down(self):
-        self.segments[0].setheading(270)
+        if self.head.heading() != 90:
+            self.head.setheading(270)
 
     def left(self):
-        self.segments[0].setheading(180)
+        if self.head.heading() != 0:
+            self.head.setheading(180)
 
     def right(self):
-        self.segments[0].setheading(0)
+        if self.head.heading() != 180:
+            self.head.setheading(0)
