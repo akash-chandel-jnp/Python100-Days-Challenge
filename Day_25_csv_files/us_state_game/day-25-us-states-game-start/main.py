0a02e1442c825249ffe915828aa538b7d7a2639a
Akash Chandel <akashsingh12345@gmail.com>
2023-11-12 03:34:46 +0530

    Added pic on the screen  as background

---
+++ Day_25_csv_files/us_state_game/day-25-us-states-game-start/main.py
@@ -0,0 +1,11 @@
+import turtle
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.title("U.S. States Game")
+image = 'blank_states_img.gif'
+screen.addshape(image)
+turtle.shape(image)
+
+
+screen.exitonclick()
